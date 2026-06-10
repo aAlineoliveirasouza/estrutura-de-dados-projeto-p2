ed28fa35387a0540536abe7b492895f6475bf7a8
aAlineoliveirasouza <alineoliveirasouza753@gmail.com>
2026-06-10 15:15:22 -0300

    Cria classe Usuario

---
+++ Usuario.java
@@ -0,0 +1,44 @@
+public class Usuario {
+    private int matricula;
+    private String nome;
+    private String email;
+
+    public Usuario(int matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(matricula).append(" - ").append(nome).append(" - ").append(email);
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Usuario u = (Usuario) o;
+        return matricula == u.matricula;
+    }
+}
