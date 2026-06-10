9d7364481a706600ace81cd351f4d3871d9e2b19
aAlineoliveirasouza <alineoliveirasouza753@gmail.com>
2026-06-10 15:08:25 -0300

    Cria classe Livro

---
+++ Livro.java
@@ -0,0 +1,75 @@
+public class Livro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anoPub;
+    private boolean disponivel;
+
+    public Livro(String isbn, String titulo, String autor, int anoPub) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPub = anoPub;
+        this.disponivel = true;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPub() {
+        return anoPub;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[")
+                .append(isbn)
+                .append("] ")
+                .append(titulo)
+                .append(" - ")
+                .append(autor)
+                .append("(")
+                .append(anoPub)
+                .append(") {")
+                .append(disponivel ? "DISPONIVEL" : "INDISPONIVEL")
+                .append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Livro l = (Livro) o;
+        return isbn != null && isbn.equals(l.isbn);
+    }
+
+}
